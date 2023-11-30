c94baf8b7d9ea3df88e33288a26746f6d0b9a0ff
Rohitlovewanshi <rohitlovewanshi78@gmail.com>
2023-11-30 15:48:33 +0530

    initial commit

---
+++ app/src/main/java/eu/tutorials/mywishlistapp/data/Wish.kt
@@ -0,0 +1,24 @@
+package eu.tutorials.mywishlistapp.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "wish-table")
+data class Wish(
+    @PrimaryKey(autoGenerate = true)
+    val id : Long = 0L,
+    @ColumnInfo(name = "wish-title")
+    val title : String = "",
+    @ColumnInfo(name = "wish-desc")
+    val description : String = ""
+)
+
+object DummyWish {
+    val wishList = listOf(
+        Wish(title = "Google Watch 1", description = "An android watch"),
+        Wish(title = "Google Watch 2", description = "An android watch"),
+        Wish(title = "Google Watch 3", description = "An android watch"),
+        Wish(title = "Google Watch 4", description = "An android watch")
+    )
+}
